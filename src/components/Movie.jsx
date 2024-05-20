1036cf86170f4710a97ccf443eaaaa56023719f5
akshat <akshatjangid14@gmail.com>
2024-05-21 04:41:52 +0530

    updated comment

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,7 +1,7 @@
-const Movie = (props) => {
+const Movie = (props) => { // props can be written in as ((img, title, year) and props will be removed from props.tile, props.year, props.img)
   return (
     <div className="movie">
-      <img src={props.img} alt="" />
+      <img src={props.img} alt="" /> 
       <p>{props.title}</p>
       <p>Year: {props.year}</p>
     </div>
